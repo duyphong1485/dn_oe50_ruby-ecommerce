af8b232397ea6537d3c3867c34322336b6913cc7
DuyPhong <duyphong1485@gmail.com>
2021-12-02 13:06:53 +0700

    Login_logout

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,26 @@
 class Account < ApplicationRecord
   has_many :oders, dependent: :destroy
   has_many :addresses, dependent: :destroy
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  before_save{email.downcase!}
+  validates :fullname, presence: true,
+                      length: {maximum: Settings.length.fullname}
+  validates :email, presence: true,
+                    length: {maximum: Settings.length.email},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: true
+  validates :password, presence: true,
+                      length: {minimum: Settings.length.password}
+
+  has_secure_password
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 end
